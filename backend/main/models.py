3df25409ec24937971f39357ed833bd9d5969448
MukunthanPeriyasamy <mukunthanperiyasamy04@gmail.com>
2025-09-10 20:13:26 +0530

    llm changed to prep

--- backend/main/models.py
+++ backend/main/models.py
@@ -13,7 +13,7 @@ os.getenv('LANGSMITH_TRACING')
 ## loading preplexity api key
 os.getenv('PPLX_API_KEY')
 # Loading the chat model
-llm = init_chat_model(model='gemini-2.5-flash',model_provider='google_genai')
+llm = init_chat_model(model='sonar',model_provider='perplexity')
 
 # Loading the embedding model from the Hugging face
 embeddings = HuggingFaceEmbeddings(model='BAAI/bge-base-en-v1.5')
