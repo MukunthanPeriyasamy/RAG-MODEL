e68c10af5f3f0aa3e1db501a28ab191a9ef984a6
MukunthanPeriyasamy <mukunthanperiyasamy04@gmail.com>
2025-09-15 20:45:50 +0530

    completed

--- backend/main/api.py
+++ backend/main/api.py
@@ -3,16 +3,31 @@ from rag import Rag_Chain
 from vectorDB import upload_document_vectorize
 from models import llm 
 import shutil
+from pydantic import BaseModel
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["*"] for dev, more secure to use exact origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class QuestionRequest(BaseModel):
+    question: str
 
 @app.get('/')
 def root():
     return 'Welcome to RAG chat'
 
 @app.post('/upload/')
-def upload(files: list[UploadFile] = File()):
+async def upload(files: list[UploadFile] = File()):
     for file in files:
         temp_file_path = f"temp_{file.filename}"
         try:
@@ -25,8 +40,10 @@ def upload(files: list[UploadFile] = File()):
             if os.path.exists(temp_file_path):
                 os.remove(temp_file_path)
 
-@app.get('/chat/{question}')    
-def chat(question):
-    result = Rag_Chain(question,llm)
-    return result
+@app.post('/chat/')    
+def chat(res : QuestionRequest):
+    result = Rag_Chain(res.question,llm)
+    return {
+        "answer": result
+    }
 
